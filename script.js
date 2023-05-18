3c627bf55dc6def0c8a8cd253f2d49ea2c248d55
Sachet Subedi <sachetsubedi001@gmail.com>
2023-05-18 23:06:14 +0545

    remoduled

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var RockButton=document.getElementById('Rbtn').addEventListener('click',rockChoo
 var PaperButton=document.getElementById('Pbtn').addEventListener('click',paperChoosed);
 var ScissorButton=document.getElementById('Sbtn').addEventListener('click',scissorChoosed);
 var result=document.getElementById('resultBox');
+var round=document.getElementById('roundCount');
+var roundCount=1,cmptrwinCount=0,userWinCount=0,drawCount=0;
  function decider()
  {
     display1.src="image/rock.png";
@@ -55,29 +57,59 @@ function scissorChoosed()
     if(cmptrChoice===userChoice)
     {
         result.innerHTML="Draw";
+        drawCount++;
     }
     if(cmptrChoice===0 && userChoice==1)
     {
         result.innerHTML="You win";
+        userWinCount++;
     }
     if(cmptrChoice==1 && userChoice==2)
     {
         result.innerHTML="You win";
+        userWinCount++;
     }
     if(cmptrChoice==2 && userChoice==0)
     {
         result.innerHTML="You win";
+        userWinCount++;
     }
     if(cmptrChoice==1 && userChoice==0)
     {
         result.innerHTML="Computer wins";
+        cmptrwinCount++;
     }
     if(cmptrChoice==2 && userChoice==1)
     {
         result.innerHTML="Computer wins";
+        cmptrwinCount++;
     }
     if(cmptrChoice==0 && userChoice==2)
     {
         result.innerHTML="Computer win";
+        cmptrwinCount++;
     }
+    roundIncr();
+    winsCounter();
+ }
+ 
+ function roundIncr()
+ {
+    roundCount++;
+    round.innerHTML="Round "+roundCount;
+ }
+ var cmptrWins=document.getElementById('cmptrWins');
+ var userWins=document.getElementById('userWins');
+ var draw=document.getElementById('draw');
+ function winsCounter()
+ {
+    cmptrWins.innerHTML="Computer wins: "+cmptrwinCount;
+    userWins.innerHTML="Your wins: "+userWinCount;
+    draw.innerHTML="Draw: "+drawCount;
+ }
+ var resetButton=document.getElementById('reset').addEventListener('click',reset);
+ 
+ function reset()
+ {
+    location.reload();
  }
